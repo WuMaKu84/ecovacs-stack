4ec95528454f1faafd319071dcdfa0eb59717b81
WuMaKu84 <95185151+WuMaKu84@users.noreply.github.com>
2026-07-17 11:23:08 +0200

    Update websocket.service.ts

--- frontend/src/services/websocket.service.ts
+++ frontend/src/services/websocket.service.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const websocketService = () => {
-  const socket = io(`ws://${process.env.REACT_APP_HOST_IP}:3000`);
+  const socket = io(`ws://${process.env.REACT_APP_HOST_IP}:${process.env.REACT_APP_WS_PORT || 3001}`);
 
   socket.on('disconnect', () => {
     console.log(socket.id);
